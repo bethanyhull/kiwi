3624804003ddbb9c8e09d05cf03880a13083eeb0
Bethany Hull <bethanyhull@u.boisestate.edu>
2021-02-04 06:52:44 -0700

    change to php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kiwi: Recipe Manager</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="landingStyle.css">
+</head>
+<body>
+    <div></div>
+    <div>
+        <h1>Kiwi</h1>
+        <p>A modern recipe manager for all your favorite recipes.</p>
+        <div>
+            <button class="button1">Register</button>
+            <button>Sign in</button>
+        </div>
+    </div>
+    
+</body>
+</html>
