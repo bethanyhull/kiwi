5dbaafb5535340f04c13bdb3a211b8521f05603c
Bethany Hull <bethanyhull@u.boisestate.edu>
2021-03-29 11:45:49 -0600

    validation bug

--- recipeLibrary.php
+++ recipeLibrary.php
@@ -17,7 +17,14 @@ session_start();
       <h1>Recipe Library</h1>
       <input type="text" />
     </div>
-    <?php echo "<h1>Hello!!!</h1>" ?>
+    <?php 
+    echo "<h1>Hello!!!</h1>";
+    require_once 'Dao.php';
+    $dao = new Dao();
+    $recipes = $dao->getRecipeLibrary($_SESSION['user_id']);
+    echo "<pre>" . print_r($recipes, 1) . "</pre>";
+    ?>
+    
     <?php
     require_once 'Dao.php';
     $dao = new Dao();
