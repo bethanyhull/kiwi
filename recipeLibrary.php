142af3e2b1c0d6a22691eb6cdc995d703c781ab9
Bethany Hull <bethanyhull@u.boisestate.edu>
2021-03-29 13:47:48 -0600

    validation bug

--- recipeLibrary.php
+++ recipeLibrary.php
@@ -19,7 +19,7 @@ echo "<pre>" . print_r($_SESSION,1) . "</pre>";
     </div>
     <?php 
     echo "<h1>Hello!!!</h1>";
-    require_once 'Dao.php';
+    require('Dao.php');
     $dao = new Dao();
     $recipes = $dao->getRecipeLibrary($_SESSION['user_id']);
     echo "<pre>" . print_r($recipes, 1) . "</pre>";
